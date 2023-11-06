963f3cb242208f7ba32a045ebc399a12f7cd2f80
Luis Gonzalez <luisarmando1234@gmail.com>
2023-11-06 04:56:41 +0000

    Adding base functionality for generating one file per class

--- tests/GeneratorTest.php
+++ tests/GeneratorTest.php
@@ -27,6 +27,30 @@ class GeneratorTest extends TestCase
         $this->assertTrue(str_contains($result, 'sub_category?: Based.TypeScript.Tests.Models.Category | null;'));
         $this->assertTrue(str_contains($result, 'products_count?: number | null;'));
 
-        unlink($output);
+        //unlink($output);
+    }
+
+    /** @test */
+    public function it_works_with_multiple_files()
+    {
+        $output = @tempnam('/tmp', '');
+
+        $generator = new TypeScriptGenerator(
+            generators: config('typescript.generators'),
+            output: $output,
+            autoloadDev: true
+        );
+
+        $generator->execute(true);
+
+        $this->assertFileExists($output);
+
+        $result = file_get_contents($output);
+
+        $this->assertEquals(3, substr_count($result, 'interface'));
+        $this->assertTrue(str_contains($result, 'sub_category?: Based.TypeScript.Tests.Models.Category | null;'));
+        $this->assertTrue(str_contains($result, 'products_count?: number | null;'));
+
+        //unlink($output);
     }
 }
